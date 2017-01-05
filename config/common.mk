d309c0019a5ee08cd8ea1fbcb940ae9a5af767e1
GreenArrow9 <catbat.pc@gmail.com>
2017-01-05 10:29:58 +0000

    vendor: Add OMS
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -37,6 +37,10 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# OMS
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.substratum.verified=true
+
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     vendor/dnd/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
@@ -130,6 +134,7 @@ PRODUCT_PACKAGES += \
     LiveLockScreenService \
     LockClock \
     WallpaperPicker \
+    masquerade \
     WeatherProvider
 
 # Exchange support
